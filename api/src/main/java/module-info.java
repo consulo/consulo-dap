c97d13e072fd48e2158991f859e59e98a02e8c9b
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 17:12:24 +0300

    drop ide api deps

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -3,7 +3,17 @@
  * @since 2024-12-22
  */
 module consulo.execution.debugger.dap {
+    requires consulo.application.api;
+    requires consulo.component.api;
     requires transitive consulo.execution.debug.api;
+    requires consulo.logging.api;
+    requires consulo.platform.api;
+    requires consulo.ui.api;
+    requires consulo.ui.ex.api;
+    requires consulo.util.collection;
+    requires consulo.util.concurrent;
+    requires consulo.util.lang;
+    requires consulo.virtual.file.system.api;
 
     exports consulo.execution.debugger.dap;
     exports consulo.execution.debugger.dap.protocol;
--- plugin/src/main/java/module-info.java
+++ plugin/src/main/java/module-info.java
@@ -5,6 +5,10 @@
 module consulo.dap {
     requires consulo.execution.debug.api;
     requires consulo.execution.debugger.dap;
+    requires consulo.logging.api;
+    requires consulo.proxy;
+    requires consulo.util.io;
+    requires consulo.util.lang;
 
     requires com.google.gson;
 
